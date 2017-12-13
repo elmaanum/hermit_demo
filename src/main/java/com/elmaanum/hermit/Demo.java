39974672b4a03101877754e16bcdafdd1a3f4251
Roger Grantham <grantham.r@gmail.com>
2017-12-13 13:41:22 -0600

    project setup

--- src/main/java/com/elmaanum/hermit/Demo.java
+++ src/main/java/com/elmaanum/hermit/Demo.java
@@ -1,4 +1,24 @@
 package com.elmaanum.hermit;
 
+import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import java.io.File;
+
 public class Demo {
+    private static final String PIZZA = "/pizza.owl";
+
+    public static void main(String[] args) throws Exception {
+        // First, we create an OWLOntologyManager object. The manager will load and save ontologies.
+        final OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
+        // We use the OWL API to load the Pizza ontology.
+        final OWLOntology ontology = ontologyManager.loadOntologyFromOntologyDocument(IRI.create(new File(PIZZA).toURI().toURL()));
+        // Now, we instantiate HermiT by creating an instance of the Reasoner class in the package org.semanticweb.HermiT.
+        final Reasoner hermit = new Reasoner(ontology);
+        // Finally, we output whether the ontology is consistent.
+        System.out.println(hermit.isConsistent());
+    }
 }
